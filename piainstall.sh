18ada2acd3730c8e74d8368ff213adb5ce340b4a
An013r <61297267+An013r@users.noreply.github.com>
2023-02-15 03:15:29 +0000

    Fixed Issues

--- piainstall.sh
+++ piainstall.sh
@@ -2,13 +2,15 @@
 
 # Download and extract
 mkdir piatmp
-wget "https://installers.privateinternetaccess.com/download/pia-linux-3.3.1-06924.run" -o "piatmp/pia.sh"
-chmod +x piatmp/pia.sh
-piatmp/pia.sh --target piatmp/ins
+wget "https://installers.privateinternetaccess.com/download/pia-linux-3.3.1-06924.run" -O "piatmp/pia.run"
+chmod +x piatmp/pia.run
+piatmp/pia.run --target piatmp/ins
 # Boot
 cp piatmp/ins/installfiles/piavpn.service /etc/systemd/system/
-systemctl enable --now piavpn
-# Exec
+systemctl enable piavpn
+# Exec and other
 cp piatmp/ins/installfiles/piavpn.desktop /usr/share/applications/
 cp piatmp/ins/installfiles/app-icon.png /usr/share/pixmaps/piavpn.png
-cp piatmp/ins/installfiles/piafiles/* /opt/piavpn/
+cp piatmp/ins/piafiles/* /opt/piavpn/
+
+groupadd piavpn
